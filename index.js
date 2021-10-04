fc07b1f520ea1a0a4470ba74d5f1572b157f7ba5
minhhoang1511sn <minhhoang1511sn@gmai.com>
2021-10-04 20:33:40 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')//đi vào thư mục nodemodule tải express rabode
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
